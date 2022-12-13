17dff7957298dc3da63476b9da8a0fea18d97c83
happifeetty#@gmail.com <tokayode@gmail.com>
2022-12-12 19:56:17 -0800

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,6 +1,12 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker");
+printf("%d\n", _strlen("My Dyn Lib"));
 return (0);
 }
